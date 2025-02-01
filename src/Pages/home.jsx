df19c4bf456ec98cd8e6252603c00a1d434879c9
Heto002 <Heto002@outlook.com>
2025-02-01 10:23:50 -0800

    Mnor changes

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -3,7 +3,7 @@ export function Home(){
     return (
         <>
             <div className="App">
-                <h1 class="home">Home</h1>
+                <h1 class="home">Homepage</h1>
             </div>
         </>
     );
